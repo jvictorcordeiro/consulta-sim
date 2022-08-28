b1681ad6b80bc89f987acb14f1594f5faa4f1dc8
jvictorcordeiro <jvictorcor@hotmail.com>
2022-08-28 17:03:07 -0300

    adicionando testes do model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criando paciente com cpf invalido' do
+    paciente = Paciente.new nome: 'Joao Victor', nascimento: '2001-03-15',
+                            cpf: '123.465.6a8-89', email: 'biruleibe@bol.com'
+    assert_not paciente.save
+  end
+  test 'Criando paciente com email invalido' do
+    paciente = Paciente.new nome: 'Joao Victor', nascimento: '2001-03-15',
+                            cpf: '123.465.6a8-89', email: 'biruleibedasilva'
+    assert_not paciente.save
+  end
 end

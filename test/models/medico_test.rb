c1a58e8048e2a9d1d796f14db38e6bd6d7680ca0
jvictorcordeiro <jvictorcor@hotmail.com>
2022-08-28 16:51:05 -0300

    adicionando testes do model de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criando medico vazio' do
+    medico = Medico.new
+    assert_not medico.save
+  end
+
+  test 'Criando medico com dados validos' do
+    medico = Medico.new nome: 'Nathalia', cpf: '20710275072',
+                        email: 'nathalia@bol.com', especialidade: 'pediatra', crm: '3015'
+    assert medico.save
+  end
 end
